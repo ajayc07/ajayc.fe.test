b15aa1e48514b58c8d1c1900b4b72e29cd244b3d
Ajay <ajaychandrasekaran8595@gmail.com>
2019-05-07 07:05:04 +0530

    UI update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function handleChanges() {
     document.getElementById("project-container").style.display = "none";
     document.getElementById("contact").style.display = "none";
+    document.getElementById("btn-home").style.backgroundColor = "#86C232";
+    document.getElementById("btn-home").style.color = "#fff";
 }
 
 function projectClicked() {
@@ -8,8 +10,14 @@ function projectClicked() {
     document.getElementById("contact").style.display = "none";
     document.getElementById("img").style.display = "none";
     document.getElementById("project-container").style.display = "block";
-    document.getElementById("portfolio").style.width = "90%";
+    document.getElementById("portfolio").style.width = "55%";
     document.getElementById("portfolio").style.height = "100%";
+    document.getElementById("btn-home").style.backgroundColor = "transparent";
+    document.getElementById("btn-proj").style.backgroundColor = "#86C232";
+    document.getElementById("btn-contact").style.backgroundColor = "transparent";
+    document.getElementById("btn-proj").style.color = "#fff";
+    document.getElementById("btn-home").style.color = "#C5C6C7";
+    document.getElementById("btn-contact").style.color = "#C5C6C7";
 }
 
 function homeClicked() {
@@ -19,6 +27,12 @@ function homeClicked() {
     document.getElementById("project-container").style.display = "none";
     document.getElementById("portfolio").style.width = "50%";
     document.getElementById("portfolio").style.height = "70%";
+    document.getElementById("btn-home").style.backgroundColor = "#86C232";
+    document.getElementById("btn-proj").style.backgroundColor = "transparent";
+    document.getElementById("btn-contact").style.backgroundColor = "transparent";
+    document.getElementById("btn-home").style.color = "#fff";
+    document.getElementById("btn-proj").style.color = "#C5C6C7";
+    document.getElementById("btn-contact").style.color = "#C5C6C7";
 
     if (window.matchMedia('screen and (max-width: 600px)').matches) {
         
@@ -34,6 +48,12 @@ function contactClicked() {
     document.getElementById("project-container").style.display = "none";
     document.getElementById("portfolio").style.width = "50%";
     document.getElementById("portfolio").style.height = "70%";
+    document.getElementById("btn-home").style.backgroundColor = "transparent";
+    document.getElementById("btn-proj").style.backgroundColor = "transparent";
+    document.getElementById("btn-contact").style.backgroundColor = "#86C232";
+    document.getElementById("btn-contact").style.color = "#fff";
+    document.getElementById("btn-home").style.color = "#C5C6C7";
+    document.getElementById("btn-proj").style.color = "#C5C6C7";
 
     if (window.matchMedia('screen and (max-width: 600px)').matches) {
         
